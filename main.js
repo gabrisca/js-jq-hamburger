3963c2447fdeca09b9a09187571428099e91c5bd
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-19 14:29:15 +0200

    option2

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 // metto tutto il mio codice nella funzione $(document).ready...
 $(document).ready(function () {
 
+  // REFERENCES /////////////////////////////////////
+
   // identifico e registro in una var la classe dell'elemento che contiene il menu hamburger
   var menu = $(".hamburger-menu");
 
@@ -11,6 +13,11 @@ $(document).ready(function () {
 
   // identifico e registro in una var la classe dell'elemento che dovrebbe chiudere il menu hamburger
   var closeHamburgerMenu = $(".close");
+
+  // END REFERENCES /////////////////////////////////////
+
+
+  // EVENTS ///////////////////////////////////
 
   // al click su openHamburgerMenu apro il menu
   openHamburgerMenu.click(function () {
@@ -23,5 +30,50 @@ $(document).ready(function () {
     console.log("chiudo il menu");
     menu.hide();
   })
+  // END EVENTS ///////////////////////////////
 
+
+  ////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////// OPTION 2 CON FUZNIONI //////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
+
+   // REFERENCES /////////////////////////////////////
+
+  // identifico e registro in una var la classe dell'elemento che contiene il menu hamburger
+  var menu = $(".hamburger-menu");
+
+  // identifico e registro in una var la classe dell'elemento che dovrebbe aprire il menu hamburger
+  var openHamburgerMenu = $(".header-right > a");
+
+  // identifico e registro in una var la classe dell'elemento che dovrebbe chiudere il menu hamburger
+  var closeHamburgerMenu = $(".close");
+
+  // END REFERENCES /////////////////////////////////////
+
+
+  // EVENTS ///////////////////////////////////
+
+  // al click su openHamburgerMenu apro il menu
+  openHamburgerMenu.click(openMenu);
+
+   // al click su closeHamburgerMenu chiudo il menu
+  closeHamburgerMenu.click(closeMenu);
+
+  // END EVENTS ///////////////////////////////
+
+
+  // FUNCTIONS ///////////////////////////////////
+
+  // Funzioni apri e chiudi menu
+  function openMenu (){
+    menu.show();
+    console.log("apro il menu");
+  }
+
+  function closeMenu (){
+    menu.hide();
+    console.log("chiudo il menu");
+  }
+
+  // END FUNCTIONS ///////////////////////////////////
 });
